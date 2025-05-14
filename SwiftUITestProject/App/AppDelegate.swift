86428621abd6540ab6b8a1157fbebe1fdbc91976
Serik Abdirov <smabdirov99@icloud.com>
2025-05-14 12:35:14 +0300

    app delegate

---
+++ SwiftUITestProject/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SwiftUITestProject
+//
+//  Created by Серик Абдиров on 14.05.2025.
+//  Copyright © 2025 Spider Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        print(#function)
+        return true
+    }
+}
--- SwiftUITestProject/App/SwiftUITestProjectApp.swift
+++ SwiftUITestProject/App/SwiftUITestProjectApp.swift
@@ -1,9 +1,12 @@
-import SwiftUI
 import Main
+import SwiftUI
 import VIPER
 
 @main
 struct SwiftUITestProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             VIPERAssembly().build()
